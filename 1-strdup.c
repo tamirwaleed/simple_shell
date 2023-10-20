9e02e42a58d13b92af97672d02a09eeef10a7dd5
tamirwaleed <hero2000.TS@gmail.com>
2023-10-20 15:14:49 -0700

    teemo

--- 1-strdup.c
+++ 1-strdup.c
@@ -6,16 +6,17 @@
  */
 char *_strdup(const char *str)
 {
-char *s;
-unsigned int i, size = 0;
-if (str == NULL)
-return (NULL);
-while (str[size] != '\0')
-size++;
-s = malloc(sizeof(char) * (size + 1));
-if (s == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-s[i] = str[i];
-return (s);
+int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
 }
